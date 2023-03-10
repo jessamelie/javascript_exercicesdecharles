a693ded2ad87f7c4524dcf88219c3e697c7c091b
jessamelie <jessamelie.sola@gmail.com>
2023-03-10 16:50:28 +0100

    test premier commit

---
+++ apollo11.js
@@ -0,0 +1,7 @@
+let compteur
+for (i=12; i>=9; i--){
+console.log(i)}
+if (i=9) console.log("Ignition sequence start")
+for (i=6; i>=0; i--){
+    console.log(i)}
+console.log("All engines running\nLiftoff!!! 🚀")
