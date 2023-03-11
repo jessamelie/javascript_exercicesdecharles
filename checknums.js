5422c2dec2f5c7c6c4492400b43998587e7be0b7
jessamelie <jessamelie.sola@gmail.com>
2023-03-11 22:06:24 +0100

    update : changement sémantique

--- checknums.js
+++ checknums.js
@@ -2,9 +2,9 @@ let num1 = 32
 let num2 = 32
 
 if (num2 > num1){
-    console.log(num2 + " est supérieur à " + num1)
+    console.log(num2 + " est supérieur au nombre " + num1)
 } else if (num2<num1) {
-    console.log(num2 + " est inférieur à " + num1)
+    console.log(num2 + " est inférieur au nombre " + num1)
 } else {
-    console.log(num1 + " et " + num2 + " sont équivalents")
+    console.log(num1 + " et " + num2 + " sont équivalents en tout point.")
 }
